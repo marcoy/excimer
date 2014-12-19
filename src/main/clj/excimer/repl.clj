4a31615027faf879604a4cc38260f55341b58ea6
Marco Yuen <marcoy@gmail.com>
2014-12-18 21:58:06 -0500

    Populate the repl namespace with convenient functions

--- src/main/clj/excimer/repl.clj
+++ src/main/clj/excimer/repl.clj
@@ -1,5 +1,43 @@
 (ns excimer.repl
   (:require [aprint.core :refer :all]
+            [clojure.reflect :refer [reflect]]
             [clojure.repl :refer :all]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [slingshot.slingshot :refer :all]))
+            [slingshot.slingshot :refer :all])
+  (:import [excimer.nrepl NREPLServer]))
+
+
+(defn get-beans
+  "Get the list of beans defined in the application context."
+  []
+  (into [] (.. NREPLServer/INSTANCE (getApplicationContext) (getBeanDefinitionNames))))
+
+(defn get-bean
+  "Get a bean from the application context."
+  [^String bean-name]
+  (.. NREPLServer/INSTANCE (getApplicationContext) (getBean bean-name)))
+
+(defn has-bean
+  "Is the bean defined in the application context."
+  [^String bean-name]
+  (.. NREPLServer/INSTANCE (getApplicationContext) (containBean bean-name)))
+
+(defn is-singleton
+  [^String bean-name]
+  (.. NREPLServer/INSTANCE (getApplicationContext) (isSingleton bean-name)))
+
+(defn is-prototype
+  [^String bean-name]
+  (.. NREPLServer/INSTANCE (getApplicationContext) (isPrototype bean-name)))
+
+(defn get-type
+  [^String bean-name]
+  (.. NREPLServer/INSTANCE (getApplicationContext) (getType bean-name)))
+
+(defn get-bean-names-from-type
+  [^Class bean-class]
+  (into [] (.. NREPLServer/INSTANCE (getApplicationContext) (getBeanNamesForType bean-class))))
+
+(defn get-env
+  []
+  (.. NREPLServer/INSTANCE (getApplicationContext) (getEnvironment)))

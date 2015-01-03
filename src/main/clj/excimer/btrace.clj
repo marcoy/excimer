3b89f548d9d2e68314276bb8628078853ffbdc5f
Marco Yuen <marcoy@gmail.com>
2015-01-02 19:11:21 -0500

    Rename btrace.session to btrace.connection.

--- src/main/clj/excimer/btrace.clj
+++ src/main/clj/excimer/btrace.clj
@@ -1,4 +1,5 @@
 (ns excimer.btrace
-  (:require [excimer.btrace.agent :as btagent]))
+  (:require [excimer.btrace.agent :as btagent])
+  (:import [java.io File]))
 
 (defonce load-agent btagent/load-agent)
--- src/main/clj/excimer/btrace/connection.clj
+++ src/main/clj/excimer/btrace/connection.clj
@@ -1,24 +1,24 @@
-(ns excimer.btrace.session
+(ns excimer.btrace.connection
   (:require [excimer.btrace.commands :refer [send-exit]])
   (:import [java.io PrintWriter ObjectInputStream ObjectOutputStream]
            [java.net Socket]))
 
-(defonce nrepl-session (atom nil))
+(defonce btrace-connection (atom nil))
 
-(defn close-session
+(defn close-connection
   [s]
   (let [{:keys [sock ois oos writer]} s]
     (do
       (send-exit oos)
-      (reset! nrepl-session nil))))
+      (reset! btrace-connection nil))))
 
-(defn new-session
-  ([agent-port] (new-session agent-port *out*))
+(defn new-connection
+  ([agent-port] (new-connection agent-port *out*))
   ([agent-port ^PrintWriter writer]
    (do
-     (when-first [s @nrepl-session]
-       (close-session s))
+     (when-first [s @btrace-connection]
+       (close-connection s))
      (let [sock (Socket. "127.0.0.1" agent-port)
            oos  (ObjectOutputStream. (.getOutputStream sock))
            ois  (ObjectInputStream. (.getInputStream sock))]
-       (reset! nrepl-session {:sock sock :oos oos :ois ois :writer nil})))))
+       (reset! btrace-connection {:sock sock :oos oos :ois ois :writer nil})))))

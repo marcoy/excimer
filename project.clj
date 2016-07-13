1dfd2538e5fce64d32b3fe16aa4b7100d35be817
Marco Yuen <marcoy@gmail.com>
2016-07-12 20:17:13 -0400

    Deps bump

--- project.clj
+++ project.clj
@@ -20,19 +20,19 @@
   :java-source-paths ["src/main/java"]
 
   :dependencies [[aprint "0.1.3"]
-                 [commons-io/commons-io "2.4"]
+                 [commons-io/commons-io "2.5"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.flatland/useful "0.11.5"]
-                 [org.slf4j/slf4j-api "1.7.14"]
+                 [org.slf4j/slf4j-api "1.7.21"]
                  [mvxcvi/puget  "1.0.0"]
                  [slingshot "0.12.2"]]
 
   :profiles {
              :provided
              {
-              :dependencies [[org.springframework/spring-context "4.2.4.RELEASE"]]
+              :dependencies [[org.springframework/spring-context "4.3.1.RELEASE"]]
              }
             })
